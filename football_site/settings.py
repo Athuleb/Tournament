0a384e40d5799adcee1472360d66001c992ccad2
BINIL-BALAN <binilbalan.e.k@gmail.com>
2026-03-03 20:10:11 +0530

    static edit

--- football_site/settings.py
+++ football_site/settings.py
@@ -127,7 +127,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
